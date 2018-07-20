d9f33d96161a8ec8f0e46435dfbf6f7db73a1e91
Andre Encarnacao <andreencar@gmail.com>
2018-07-20 17:09:52 +0100

    Added input and submit button to Chatbox

--- client/src/components/Chatbox.js
+++ client/src/components/Chatbox.js
@@ -1,11 +1,19 @@
+// @flow
+
 import React, { Component } from 'react';
 import '../styles/Chatbox.css';
 
-class Chatbox extends Component {
+type ChatboxProps = {
+  handleSubmitMesssage : (message : string) => any
+}
+
+class Chatbox extends Component<ChatboxProps> {
 
   render() {
     return (
       <div className="Chatbox">
+        <input type="text" className={"Chatbox--input"} />
+        <button className={"Chatbox--button"}>SEND</button>
       </div>
     );
   }
