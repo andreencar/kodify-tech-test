9c1ea3b2538701471cb927cc8d373dcaf68c9865
Andre Encarnacao <andreencar@gmail.com>
2018-07-20 17:25:37 +0100

    Added Functionality to Submit Message

--- client/src/action-creators/ChatActionCreator.js
+++ client/src/action-creators/ChatActionCreator.js
@@ -1,11 +1,23 @@
 // @flow
+import { Message, ChatState } from '../types/types';
+import generateUUID from 'uuid/v4';
+
 export function handleSubmitMessage(payload : any) {
-    return async () => {
-        await fetch("http://localhost:8080/api/chat"), {
+    return async (dispatch : any, getState : () => ChatState ) => {
+        const messageToSubmit : Message = {
+            messageId : generateUUID(),
+            userId : getState().currentUserId,
+            value : payload
+        };
+        await fetch("http://localhost:8080/api/chat", {
             method : "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
             body : JSON.stringify({
-                message : payload
+                message : messageToSubmit
             })
-        }
+        });
     };
 }
