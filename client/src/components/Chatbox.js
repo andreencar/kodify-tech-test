91bf76bdebe271b18aa0d2371182e7145ce1b8d2
Andre Encarnacao <andreencar@gmail.com>
2018-07-20 16:07:47 +0100

    Added styles folder and render functionality to placeholder components

---
+++ client/src/components/Chatbox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Chatbox.css';
+
+class Chatbox extends Component {
+
+  render() {
+    return (
+      <div className="Chatbox">
+      </div>
+    );
+  }
+}
+
+export default Chatbox;
---
+++ client/src/components/Message.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Message.css';
+
+class Message extends Component {
+
+  render() {
+    return (
+      <div className="Message">
+      </div>
+    );
+  }
+}
+
+export default Message;
