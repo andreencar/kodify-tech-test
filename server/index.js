1f07ec8ef50e9ac5d2b85e968ea11f817bdf2a16
Andre Encarnacao <andreencar@gmail.com>
2018-07-20 15:41:46 +0100

    Post Message and Publish on SSE subscribers working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const SSEChannel = require('sse-pubsub');
 
 const router = express.Router();
 const app = express();
+app.use(bodyParser.json());
+
 const channel = new SSEChannel();
 
 router.post("/chat/", function (req, res) {
